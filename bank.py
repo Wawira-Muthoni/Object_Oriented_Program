0bbac848892254734406dbdf6d9f35486f7d88d1
Wawira-Muthoni <christinewawiramuthoni@gmail.com>
2022-06-09 21:37:39 +0300

    changes

--- bank.py
+++ bank.py
@@ -1,17 +1,37 @@
 # Add these methods to class Account - deposit, withdraw. 
 # Create two instances of account and verify they work as expected.
 
-class Bank:
-    Bank_name = "Cooperative"
-    def __init__(self,balance,deposit,withdraw,account_number,name):
-        self.balance = balance+deposit - withdraw
-        self.deposit = deposit
-        self.withdraw = withdraw
-        self.account_number = account_number
+            
+class Account:
+    def __init__(self,name,acc_number):
+        self.balance = 0
         self.name = name
+        self.acc_number = acc_number
+        self.deposits = []
+        self.withdrawals = []
+    def deposit (self,amount):
+        if amount<=0:
+            return f"Deposit amount should be more than zero"
+        else:
+            self.balance+=amount
+            self.deposits.append(amount)
+            return f"You've deposited {amount}.Your new balance is {self.balance}"  
 
+    def withdraw (self,amount):
+        if amount > self.balance:
+           return  f"Your balance is {self.balance}, you cannot withdraw the {amount}"
+        elif amount <= 0:
+            return f"Amount must be greater than zero"
+        else:
+            self.balance -=amount
+            self.withdrawals.append(amount)
+            return f"You have withdrawn {amount} your balance is {self.balance}"
 
+    def deposits_statement(self):
 
-    def bank(self):
-        return f"Hello {self.name} your account number is {self.account_number} you deposited {self.deposit} and withdrawn {self.withdraw} and your balance is {self.balance}"
+        print(*self.deposits, sep="\n")
+
+    def withdrawals_statement(self):
+        print(*self.withdrawals, sep="\n")
+    
             
--- student.py
+++ student.py
@@ -1,18 +1,12 @@
 # Add these methods to class student - full_name, year_of_birth, initials. 
 # Create two instances and verify the work as expected
-
-
-
-
 class Student:
-     school = "Cooperative"
      def __init__(self,country,first_name,second_name,age):
         self.first_name = first_name
         self.country = country
         self.second_name = second_name
         self.age = age
      def full_name (self):
-
          return f"Hello {self.first_name} {self.second_name} How is {self.country}"
      def year_of_birth (self):  
          year=2022 - self.age
